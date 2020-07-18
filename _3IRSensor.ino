2152c997dad0629c05fd208658dad27ab2b78eff
SPM-dotcom <54461564+SPM-dotcom@users.noreply.github.com>
2020-07-18 15:13:12 +0530

    Update _3IRSensor.ino

--- _3IRSensor.ino
+++ _3IRSensor.ino
@@ -1,18 +1,22 @@
+//locate your IR Sensor pin 
   int ir1=8,ir2=9,ir3=10;
 void setup() {
+   // put your setup code here, to run once:
   pinMode(8,INPUT);
   pinMode(9,INPUT);
   pinMode(10,INPUT);
-  pinMode(2,INPUT);
-  pinMode(3,INPUT);
-  pinMode(4,INPUT);
-  pinMode(5,INPUT);
+  pinMode(2,OUTPUT);
+  pinMode(3,OUTPUT); //Motor Pins (Forword & Backword)
+  pinMode(4,OUTPUT);
+  pinMode(5,OUTPUT);
   Serial.begin(9600);
-  // put your setup code here, to run once:
+ 
 
 }
 
 void loop() {
+   // put your main code here, to run repeatedly:
+
   if((digitalRead(8)==HIGH && digitalRead(9)==LOW && digitalRead(10)==HIGH)|| (digitalRead(8)==LOW && digitalRead(9)==HIGH && digitalRead(10)==LOW)){
     digitalWrite(2,LOW);
     digitalWrite(3,HIGH);//Forword(1,0,1)&(0,1,0)
@@ -37,6 +41,5 @@ else if((digitalRead(8)==HIGH && digitalRead(9)==LOW && digitalRead(10)==LOW) ||
     digitalWrite(4,LOW);
     digitalWrite(5,LOW);
   }
- // put your main code here, to run repeatedly:
 
 }
